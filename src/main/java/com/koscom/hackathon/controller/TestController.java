5c0a65cae2df8819c050a69f000a9055f90d3be4
resource777 <79133019+resource777@users.noreply.github.com>
2024-01-14 14:05:41 +0900

    Chore: controller test

---
+++ src/main/java/com/koscom/hackathon/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.koscom.hackathon.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@RequestMapping(value = "/")
+	public String home(){
+
+		return "hello-home";
+	}
+
+}
