bd55c4b01bf9ca78b14fd0007ad06b7581661823
lsjpjs1 <lsjpjs1@naver.com>
2024-01-15 21:00:58 +0900

    chore: 배포 테스트

--- src/main/java/com/koscom/hackathon/controller/TestController.java
+++ src/main/java/com/koscom/hackathon/controller/TestController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 	@GetMapping("/home")
 	public String home(){
-		return "hello-home!!!!";
+		return "배포테스트!!!!";
 	}
 
 }
