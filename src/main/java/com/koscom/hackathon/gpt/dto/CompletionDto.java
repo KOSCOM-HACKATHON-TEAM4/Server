bb7b1317242a71461aa0f6b133efeca3499e448b
resource777 <79133019+resource777@users.noreply.github.com>
2024-01-30 09:52:42 +0900

    Fix: gpt 모델고정

--- src/main/java/com/koscom/hackathon/gpt/dto/CompletionDto.java
+++ src/main/java/com/koscom/hackathon/gpt/dto/CompletionDto.java
@@ -8,16 +8,16 @@ import lombok.*;
 public class CompletionDto {
 
 	// 사용할 모델
-	private String model;
+	private String model="babbage-002";
 
 	// 사용할 프롬프트 명령어
 	private String prompt;
 
 	// 프롬프트의 다양성을 조절할 명령어(default : 1)
-	private float temperature = 1;
+	private float temperature = (float)(0.7);
 
 	// 최대 사용할 토큰(default : 16)
-	private int max_tokens = 16;
+	private int max_tokens = 100;
 
 	@Builder
 	public CompletionDto(String model, String prompt, float temperature, int max_tokens) {
